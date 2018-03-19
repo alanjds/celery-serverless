6b7c67d8ec52b1e73ff60345103c07d5d43ab1c9
Alan Justino <alan.justino@yahoo.com.br>
2018-03-19 18:39:18 -0300

    Handle `celery serverless` and `celery-serverless` calls

--- celery_serverless/cli.py
+++ celery_serverless/cli.py
@@ -17,7 +17,26 @@ logger.setLevel('DEBUG')
 
 
 ## TODO: Handle the `celery serverless --help` call
-@click.command()
+@click.group(no_args_is_help=True, context_settings=dict(allow_interspersed_args=True))
+@click_handle_celery_options(accept_extra=False)
+def serverless(*args, **kwargs):
+    pass
+
+
+@serverless.command('serverless')
+@click_handle_celery_options
+@click.pass_context
+def _serverless(ctx, *args, **kwargs):
+    """
+    Boilerplate to handle the `celery serverless` command.
+    Should not be used by humans on the CLI directly.
+    """
+    logger.debug('_serverless:\n\tctx: %s \n\targs: %s \n\tkwargs: %s \n\tctx.obj: %s\n', ctx, args, kwargs, ctx.obj)
+    sys.argv.pop(sys.argv.index('serverless'))  # Call again w/ one depth less
+    sys.exit(serverless.main(prog_name='celery serverless', standalone_mode=True, obj=kwargs, allow_interspersed_args=True))
+
+
+@serverless.command()
 @click_handle_celery_options
 @click.pass_context
 def serverless(ctx, *args, **kwargs):
@@ -29,12 +48,12 @@ def serverless(ctx, *args, **kwargs):
 
 class MainCommand(Command):
     def add_arguments(self, parser):
-        parser.add_argument('options', nargs='*')   # catch-all to let Click handle the meat
+        parser.add_argument('subcommand', nargs='*')   # catch-all to let Click handle the meat
         # parser.add_argument('--spam', nargs='?')
 
     def run(self, *args, **kwargs):
         logger.debug('MainCommand:run:\n\tself: %s \n\targs: %s \n\tkwargs: %s \n' % (self, args, kwargs))
-        sys.exit(serverless.main(obj=kwargs))
+        sys.exit(serverless.main(standalone_mode=True, obj=kwargs, allow_interspersed_args=True))
 
 
 main = serverless
