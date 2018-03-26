9e2cfc7d80d88c7e5da5ebacbf747bd4da68c31f
Alan Justino <alan.justino@yahoo.com.br>
2018-03-26 13:31:13 -0300

    Factored out the worker management

--- celery_serverless/handler.py
+++ celery_serverless/handler.py
@@ -1,55 +1,11 @@
 # coding: utf-8
-import os
-import signal
 import json
 import logging
 
-import celery.bin.celery
-from celery.signals import task_prerun
+from .worker_management import spawn_worker, attach_hooks
 
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
-
-
-def _spawn_worker(softlimit:'seconds'=None, hardlimit:'seconds'=None):
-    command_argv = [
-        'celery',
-        'worker',
-        '--app project',
-        '--loglevel debug',
-        '--concurrency 1',
-        '--prefetch-multiplier 1',
-        '--without-gossip',
-        '--without-mingle',
-        '--task-events',
-        '-O fair',
-    ]
-
-    if softlimit:
-        command_argv.append('--soft-time-limit %s' % softlimit)
-    if hardlimit:
-        command_argv.append('--time-limit %s' % hardlimit)
-
-    return celery.bin.celery.main(command_argv)  # will block.
-
-
-def _attach_hooks():
-    """
-    Register the needed hooks:
-    - One to store the worker class after start
-    - One to trigger stop getting tasks after the 1st, and shutdown when done
-    """
-    @task_prerun.connect
-    def _shutdown_when_done(*args, **kwargs):
-        # After got a task, trigger Ctrl+C
-        # The worker will stop getting tasks and will prepare to shutdown
-        # but will wait the existing task to finish its lifetime.
-        pid = os.getppid()  # Should hit my manager process. Not me, the minion process
-        os.kill(pid, signal.SIGINT)  # Trigger Ctrl+C behaviours
-
-    # Return the hooks to be hold somewhere. Else it could be garbage collected
-    hooks = (_shutdown_when_done,)
-    return hooks
 
 
 def worker(event, context):
@@ -59,7 +15,8 @@ def worker(event, context):
     softlimit = remaining_seconds-30.0  # Poke the job 30sec before the abyss
     hardlimit = remaining_seconds-15.0  # Kill the job 15sec before the abyss
 
-    _spawn_worker(
+    hooks = attach_hooks()
+    spawn_worker(
         softlimit=softlimit if softlimit > 5 else None,
         hardlimit=hardlimit if hardlimit > 5 else None,
     )  # Will block until one task got processed
@@ -68,12 +25,4 @@ def worker(event, context):
         "message": "Celery worker worked, lived, and died.",
     }
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return body
-    """
+    return {"statusCode": 200, "body": json.dumps(body)}
---
+++ celery_serverless/worker_management.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+import os
+import signal
+import logging
+
+import celery.bin.celery
+from celery.signals import task_prerun
+
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+
+def spawn_worker(softlimit:'seconds'=None, hardlimit:'seconds'=None):
+    command_argv = [
+        'celery',
+        'worker',
+        '--app', 'project',
+        '--loglevel', 'debug',
+        '--concurrency', '1',
+        '--prefetch-multiplier', '1',
+        '--without-gossip',
+        '--without-mingle',
+        '--task-events',
+        '-O', 'fair',
+    ]
+
+    if softlimit:
+        command_argv.extend(['--soft-time-limit', '%s' % softlimit])
+    if hardlimit:
+        command_argv.extend(['--time-limit', '%s' % hardlimit])
+
+    logger.info('Starting the worker(s)')
+    logger.debug('Command: %s', ' '.join(command_argv))
+    return celery.bin.celery.main(command_argv)  # will block.
+
+
+def shutdown_when_done(*args, **kwargs):
+    # After got a task, trigger Ctrl+C
+    # The worker will stop getting tasks and will prepare to shutdown
+    # but will wait the existing task to finish its lifetime.
+    logger.debug('Informing the worker to stop accepting tasks')
+    pid = os.getppid()  # Should hit my manager process. Not me, the minion process
+    os.kill(pid, signal.SIGINT)  # Trigger Ctrl+C behaviours
+
+
+def attach_hooks():
+    """
+    Register the needed hooks:
+    - One to trigger stop getting tasks after the 1st, and shutdown when done
+    """
+    logger.debug('Attaching hooks')
+
+    # Return the hooks to be hold somewhere. Else it could be garbage collected
+    hooks = []
+    hooks.append(task_prerun.connect(shutdown_when_done))
+    return hooks
