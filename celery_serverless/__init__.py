e7425794eab52e9b32730f2dc0c3efd4c73e9836
Alan Justino <alan.justino@yahoo.com.br>
2018-03-28 18:59:12 -0300

    [HACK] `sls invoke local` does not work on nested modules handler

--- celery_serverless/__init__.py
+++ celery_serverless/__init__.py
@@ -5,3 +5,8 @@
 __author__ = """Alan Justino & Samuel Barbosa Neto"""
 __email__ = 'alan.justino@yahoo.com.br'
 __version__ = '0.1.0'
+
+
+# Hack: Allow `sls invoke local` to work correctly
+from .handler import worker as handler_worker
+
