05577f8612aa6ffe6b06127190ca4223f5670910
Alan Justino <alan.justino@yahoo.com.br>
2018-04-02 18:58:14 -0300

    Two timeouts: 1) Broker connection 2) Job reception

--- celery_serverless/worker_management.py
+++ celery_serverless/worker_management.py
@@ -2,9 +2,10 @@
 import os
 import signal
 import logging
+from functools import partial
 
 import celery.bin.celery
-from celery.signals import task_prerun
+from celery.signals import celeryd_init, worker_ready
 
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
@@ -48,22 +49,38 @@ def spawn_worker(softlimit:'seconds'=None, hardlimit:'seconds'=None, **options):
 
 
 def shutdown_when_done(*args, **kwargs):
-    # After got a task, trigger Ctrl+C
+    # Start to shutdown by triggering <Ctrl+C>
     # The worker will stop getting tasks and will prepare to shutdown
     # but will wait the existing task to finish its lifetime.
-    logger.debug('Informing the worker to stop accepting tasks')
-    pid = os.getppid()  # Should hit my manager process. Not me, the minion process
+    logger.debug('Informing the workers to stop accepting tasks')
+    pid = os.getpid()
     os.kill(pid, signal.SIGINT)  # Trigger Ctrl+C behaviours
 
 
-def attach_hooks():
+def wakeme_soon(handler:'callable'=None, delay:'seconds'=1.0, reason='', *args, **kwargs):
+    # After born, wait some seconds and get a UNIX signal poke.
+    if reason:
+        reason = 'waiting for "%s"' % reason
+    logger.debug('Registering an ALRM for %.2f seconds ahead %s', delay, reason)
+    signal.signal(signal.SIGALRM, handler)
+    signal.setitimer(signal.ITIMER_REAL, delay)
+
+
+def attach_hooks(wait_connection=4.0, wait_job=1.0):
     """
     Register the needed hooks:
     - One to trigger stop getting tasks after the 1st, and shutdown when done
     """
-    logger.debug('Attaching hooks')
+    logger.info('Attaching hooks')
+    logger.debug('Wait connection time: %.2f', wait_connection)
+    logger.debug('Wait job time: %.2f', wait_job)
+
+    timeout = partial(wakeme_soon, handler=shutdown_when_done)
 
     # Return the hooks to be hold somewhere. Else it could be garbage collected
     hooks = []
-    hooks.append(task_prerun.connect(shutdown_when_done))
+    # After worker process up: celeryd_init
+    hooks.append(celeryd_init.connect(partial(timeout, delay=wait_connection, reason='broker connection')))
+    # After broker queue connected: worker_ready
+    hooks.append(worker_ready.connect(partial(timeout, delay=wait_job, reason='job to come')))
     return hooks
