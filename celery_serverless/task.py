375f5745bae7ca436e03b2cd14a1a1ad27953f8b
Alan Justino <alan.justino@yahoo.com.br>
2018-04-04 17:11:54 -0300

    Warn about triggering for queues probable not listened

--- celery_serverless/task.py
+++ celery_serverless/task.py
@@ -1,13 +1,22 @@
 # coding: utf-8
 from __future__ import unicode_literals, absolute_import
+import logging
 
-from celery import Task
+from celery import Task, current_app as app
 
 from celery_serverless import invoker
+
+logger = logging.getLogger(__name__)
+
+SERVERLESS_QUEUES = app.conf.get('serverless_queues', (app.conf.task_queues or [app.conf.task_default_queue]))
 
 
 def trigger_invoke(task, *args, **kwargs):
     """Invokes the Serverless Function"""
+    if 'queue' in kwargs and kwargs['queue'] not in SERVERLESS_QUEUES:
+        logging.warning("Serverless worker will probable not get the task,"
+                        " as its queue %s is probable not being listened there",
+                        kwargs['queue'])
     return invoker.invoke_main()
 
 
