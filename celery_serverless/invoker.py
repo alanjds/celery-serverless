d792dcc398ed160a9d7fabb01d031d8454f06e41
Alan Justino <alan.justino@yahoo.com.br>
2018-04-10 18:46:15 -0300

    Object-Oriented version

--- celery_serverless/invoker.py
+++ celery_serverless/invoker.py
@@ -32,101 +32,102 @@ from .cli_utils import run
 CELERY_HANDLER_PATH = 'celery_serverless.handler_worker'
 
 
-def invoke_main(strategy=''):
-    config = get_config()
-    if not strategy:
-        strategy = _infer_strategy(config)
+class Invoker(object):
+    def __init__(self, config=None):
+        self.config = config or get_config()
 
-    logger.info("Invoke strategy selected: '%s'", strategy)
-    if strategy == 'serverless':
-        invoker = _invoke_serverless
-    elif strategy == 'boto3':
-        invoker = _invoke_boto3
-    else:
-        raise NotImplementedError("Could not find a way to invoke via '%s' strategy" % strategy)
+    def invoke_main(self, strategy=''):
+        if not strategy:
+            strategy = self._infer_strategy(self.config)
 
-    try:
-        logs = invoker(config)  # Should raise exception on some problem
-    except RuntimeError as err:
-        if logger.getEffectiveLevel() <= logging.INFO:
-            logger.info('Invocation failed via "%s": %s', strategy, err.details)
-        raise
-    return True
-
-
-def _infer_strategy(config):
-    if config['provider']['name'] == 'aws':
-        if lambda_client:
-            return 'boto3'
+        logger.info("Invoke strategy selected: '%s'", strategy)
+        if strategy == 'serverless':
+            invoker = self._invoke_serverless
+        elif strategy == 'boto3':
+            invoker = self._invoke_boto3
         else:
-            logger.warning("Invoke strategy 'boto3' could not be used. Falling back to 'serverless'")
-    return 'serverless'
+            raise NotImplementedError("Could not find a way to invoke via '%s' strategy" % strategy)
+
+        try:
+            logs = invoker()  # Should raise exception on some problem
+        except RuntimeError as err:
+            logger.info('Invocation failed via "%s": %s', strategy, err.details)
+        return True
 
 
-def _invoke_serverless(config, local=False):
-    name = _get_serverless_name(config)
-    command = 'serverless invoke'
-    if local:
-        command += ' local'
+    def _infer_strategy(self):
+        if self.config['provider']['name'] == 'aws':
+            if lambda_client:
+                return 'boto3'
+            else:
+                logger.warning("Invoke strategy 'boto3' could not be used. Falling back to 'serverless'")
+        return 'serverless'
 
-    logger.debug("Invoking via 'serverless'")
-    command += ' --log --verbose --function %s' % name
-    output = BytesIO()
-    for line, retcode in run(command, output):
-        pass    # click.echo(line, nl=False)
 
-    if logger.getEffectiveLevel() <= logging.DEBUG:
+    def _invoke_serverless(self, local=False):
+        name = _get_serverless_name(self.config)
+        command = 'serverless invoke'
+        if local:
+            command += ' local'
+
+        logger.debug("Invoking via 'serverless'")
+        command += ' --log --verbose --function %s' % name
+        output = BytesIO()
+        for line, retcode in run(command, output):
+            pass    # click.echo(line, nl=False)
+
+        if logger.getEffectiveLevel() <= logging.DEBUG:
+            output.seek(0)
+            logger.debug("Invocation logs from 'serverless':\n%s", output.read().decode())
+
+        if retcode != 0:
+            error = RuntimeError('Invocation failed on Serverless: %s' % command)
+
+            output.seek(0)
+            details = dirtyjson.loads(output.read().decode())
+            if isinstance(details, dict):
+                details = dict(details)
+            elif isinstance(details, list):
+                details = list(details)
+
+            error.logs = output
+            error.details = details
+            raise error
         output.seek(0)
-        logger.debug("Invocation logs from 'serverless':\n%s", output.read().decode())
+        return output
 
-    if retcode != 0:
-        error = RuntimeError('Invocation failed on Serverless: %s' % command)
+
+    def _invoke_boto3(self):
+        lambda_arn = _get_awslambda_arn(CELERY_HANDLER_PATH)
+        logger.debug("Invoking via 'boto3'")
+        response = lambda_client.invoke(
+            FunctionName=lambda_arn,
+            InvocationType='RequestResponse', # 'RequestResponse'|'Event'|'DryRun'
+            LogType='Tail',  # 'None'|'Tail'
+            #ClientContext='string',
+            #Payload=b'bytes'|file,
+            #Qualifier='$LATEST',  # 'string'
+        )
+
+        if logger.getEffectiveLevel() <= logging.DEBUG:
+            log_output = pformat(response)
+            logger.debug("Invocation response from 'boto3':\n%s", response)
+
+        output = BytesIO(codecs.decode(response['LogResult'].encode(), 'base64'))
+
+        if logger.getEffectiveLevel() <= logging.DEBUG:
+            output.seek(0)
+            logger.debug("Invocation logs from 'boto3':\n%s", output.read().decode())
+
+        if 'FunctionError' in response:
+            output.seek(0)
+            error = RuntimeError('Invocation failed on AWS Lambda: %s' % lambda_arn)
+            error.logs = output
+            error.details = json.load(response['Payload'])
+            raise error
 
         output.seek(0)
-        details = dirtyjson.loads(output.read().decode())
-        if isinstance(details, dict):
-            details = dict(details)
-        elif isinstance(details, list):
-            details = list(details)
-
-        error.logs = output
-        error.details = details
-        raise error
-    output.seek(0)
-    return output
-
-
-def _invoke_boto3(config):
-    lambda_arn = _get_awslambda_arn(CELERY_HANDLER_PATH)
-    logger.debug("Invoking via 'boto3'")
-    response = lambda_client.invoke(
-        FunctionName=lambda_arn,
-        InvocationType='RequestResponse', # 'RequestResponse'|'Event'|'DryRun'
-        LogType='Tail',  # 'None'|'Tail'
-        #ClientContext='string',
-        #Payload=b'bytes'|file,
-        #Qualifier='$LATEST',  # 'string'
-    )
-
-    if logger.getEffectiveLevel() <= logging.DEBUG:
-        log_output = pformat(response)
-        logger.debug("Invocation response from 'boto3':\n%s", response)
-
-    output = BytesIO(codecs.decode(response['LogResult'].encode(), 'base64'))
-
-    if logger.getEffectiveLevel() <= logging.DEBUG:
-        output.seek(0)
-        logger.debug("Invocation logs from 'boto3':\n%s", output.read().decode())
-
-    if 'FunctionError' in response:
-        output.seek(0)
-        error = RuntimeError('Invocation failed on AWS Lambda: %s' % lambda_arn)
-        error.logs = output
-        error.details = json.load(response['Payload'])
-        raise error
-
-    output.seek(0)
-    return output
+        return output
 
 
 def _get_serverless_name(config):
