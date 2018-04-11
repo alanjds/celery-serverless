ce9aa21acc43da8ff134832789595ef4622a11ed
Alan Justino <alan.justino@yahoo.com.br>
2018-04-11 19:01:51 -0300

    Simpler mixin implementation

--- celery_serverless/task.py
+++ celery_serverless/task.py
@@ -11,25 +11,24 @@ logger = logging.getLogger(__name__)
 SERVERLESS_QUEUES = app.conf.get('serverless_queues', (app.conf.task_queues or [app.conf.task_default_queue]))
 
 
-def trigger_invoke(task, *args, **kwargs):
+def trigger_invoke(task=None, *args, **kwargs):
     """Invokes the Serverless Function"""
     if 'queue' in kwargs and kwargs['queue'] not in SERVERLESS_QUEUES:
         logging.warning("Serverless worker will probable not get the task,"
                         " as its queue %s is probable not being listened there",
                         kwargs['queue'])
-    return invoker.invoke_main()
+    return invoker.Invoker().invoke_main()
 
 
-class TriggerAfterQueueTask(Task):
+class TriggerServerlessMixin(object):
     """Do the standart Task enqueue, then invokes the Serverless Function"""
     def apply_async(self, *args, **kwargs):
-        result = super(__class__, self).apply_async(self, *args, **kwargs)
-        trigger_invoke(self, *args, **kwargs)
-        return result
+        return trigger_invoke(self, *args, **kwargs)
 
 
-class TriggerBeforeQueueTask(Task):
+class TriggerAfterQueueTask(Task, TriggerServerlessMixin):
+    """Do the standart Task enqueue, then invokes the Serverless Function"""
+
+
+class TriggerBeforeQueueTask(TriggerServerlessMixin, Task):
     """Invokes the Serverless Function, then do the standard Task enqueue"""
-    def apply_async(self, *args, **kwargs):
-        trigger_invoke(self, *args, **kwargs)
-        return super(__class__, self).apply_async(self, *args, **kwargs)
