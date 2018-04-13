6cd8080292d5b80c525749304483cf00b4f5ffe0
Alan Justino <alan.justino@yahoo.com.br>
2018-04-13 15:59:42 -0300

    PEP-8: 4 spaces!

--- celery_serverless/handler.py
+++ celery_serverless/handler.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 try:  # https://github.com/UnitedIncome/serverless-python-requirements#dealing-with-lambdas-size-limitations
-  import unzip_requirements
+    import unzip_requirements
 except ImportError:
-  pass
+    pass
 
 import json
 import logging
