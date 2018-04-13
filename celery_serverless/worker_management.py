6c189432c3c94b4d31115982291d5b60bd665082
Alan Justino <alan.justino@yahoo.com.br>
2018-04-13 16:24:27 -0300

    Inform the shutdown reason

--- celery_serverless/worker_management.py
+++ celery_serverless/worker_management.py
@@ -52,7 +52,8 @@ def shutdown_when_done(*args, **kwargs):
     # Start to shutdown by triggering <Ctrl+C>
     # The worker will stop getting tasks and will prepare to shutdown
     # but will wait the existing task to finish its lifetime.
-    logger.debug('Informing the workers to stop accepting tasks')
+    reason = kwargs.get('reason', '')
+    logger.debug('Informing the workers to stop accepting tasks. %s', reason)
     pid = os.getpid()
     os.kill(pid, signal.SIGINT)  # Trigger Ctrl+C behaviours
 
@@ -77,7 +78,8 @@ def attach_hooks(wait_connection=4.0, wait_job=1.0):
 
     @celeryd_init.connect  # After worker process up
     def _broker_connection_timeout(*args, **kwargs):
-        return wakeme_soon(reason='broker connection', delay=wait_connection, callback=shutdown_when_done)
+        _shutdown = partial(shutdown_when_done, reason='Broker never connected')
+        return wakeme_soon(reason='broker connection', delay=wait_connection, callback=_shutdown)
 
     @worker_ready.connect  # After broker queue connected
     def _worker_ready_timeout(*args, **kwargs):
