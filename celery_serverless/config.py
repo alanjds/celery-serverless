7c9a8316090f444745b1aea2f94c108b41082ad0
Alan Justino <alan.justino@yahoo.com.br>
2018-04-25 16:17:50 -0300

    Saves ~50 ms

--- celery_serverless/config.py
+++ celery_serverless/config.py
@@ -1,12 +1,14 @@
 #coding: utf-8
 from __future__ import unicode_literals, absolute_import
 import os
+import functools
 from pathlib import Path
 
 from ruamel.yaml import YAML
 yaml = YAML()
 
 
+@functools.lru_cache(1)
 def get_config(file_path='./serverless.yml'):
     if not os.path.exists(file_path):
         raise RuntimeError(f"No file '{file_path}' detected."
