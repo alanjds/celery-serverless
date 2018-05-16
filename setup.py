8ad50c1245e3b52ca5a176732d88cc0a0d8797ac
Alan Justino <alan.justino@yahoo.com.br>
2018-05-16 12:44:43 -0300

    setup.py: 's3conf' extra added

--- setup.py
+++ setup.py
@@ -56,6 +56,9 @@ setup(
         'wdb': [
             'wdb>=3.2.1',
         ],
+        's3conf': [
+            's3conf>=0.6.0',
+        ],
     },
     license="Apache Software License 2.0",
     long_description=readme,
