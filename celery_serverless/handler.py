0688223d132011fb469aa39e3066e1173232acc2
Alan Justino <alan.justino@yahoo.com.br>
2018-06-04 17:20:56 -0300

    Simpler worker handler, via @handler_wrapper()

--- celery_serverless/handler.py
+++ celery_serverless/handler.py
@@ -8,7 +8,7 @@ import os
 import sys
 import logging
 
-from .handler_utils import maybe_call_hook, import_callable, ENVVAR_NAMES as ENVVARS
+from .handler_utils import maybe_call_hook, import_callable, handler_wrapper, ENVVAR_NAMES
 
 logger = logging.getLogger(__name__)
 logger.propagate = True
@@ -18,89 +18,54 @@ print('Celery serverless loglevel:', logger.getEffectiveLevel())
 
 
 ### 1st hook call
-maybe_call_hook(ENVVARS['pre_warmup_envvar'], locals())
+maybe_call_hook(ENVVAR_NAMES['pre_warmup_envvar'], locals())
 
 # Get and activate some extras
-from celery_serverless.extras import discover_extras, maybe_apply_sentry
+from celery_serverless.extras import discover_extras
 available_extras = discover_extras()
 print('Available extras:', list(available_extras.keys()), file=sys.stderr)
 
-import json
 from celery_serverless.worker_management import spawn_worker, attach_hooks
 
 hooks = []
 
 ### 2nd hook call
-maybe_call_hook(ENVVARS['pre_handler_definition', locals())
+maybe_call_hook(ENVVAR_NAMES['pre_handler_definition'], locals())
 
 
-@maybe_apply_sentry(available_extras)
+@handler_wrapper(available_extras)
 def worker(event, context):
     global hooks
 
-    request_id = '(unknown)'
-    if 'wdb' in available_extras:
-        available_extras['wdb']['start_trace']()
-        # Will be True if CELERY_SERVERLESS_BREAKPOINT is defined.
-        # It is the preferred way to force a breakpoint.
-        if available_extras['wdb']['breakpoint']:
-            import wdb
-            wdb.set_trace()  # Tip: you may want to step into spawn_worker() near line 100 ;)
-
     try:
-        ### 4th hook call
-        maybe_call_hook(ENVVARS['pre_handler_call'], locals())
-
-        try:
-            request_id = context.aws_request_id
-        except AttributeError:
-            pass
-        logger.info('START: Handle request ID: %s', request_id)
-
-        try:
-            remaining_seconds = context.get_remaining_time_in_millis() / 1000.0
-        except Exception as e:
-            logger.exception('Could not got remaining_seconds. Is the context right?')
-            remaining_seconds = 5 * 60 # 5 minutes by default
-
-        softlimit = remaining_seconds-30.0  # Poke the job 30sec before the abyss
-        hardlimit = remaining_seconds-15.0  # Kill the job 15sec before the abyss
-
-        if not hooks:
-            logger.debug('Fresh Celery worker. Attach hooks!')
-            hooks = attach_hooks()
-        else:
-            logger.debug('Old Celery worker. Already have hooks.')
-
-        # The Celery worker will start here. Will connect, take 1 (one) task,
-        # process it, and quit.
-        logger.debug('Spawning the worker(s)')
-        spawn_worker(
-            softlimit=softlimit if softlimit > 5 else None,
-            hardlimit=hardlimit if hardlimit > 5 else None,
-            loglevel='DEBUG',
-        )  # Will block until one task got processed
-
-        logger.debug('Cleaning up before exit')
-        body = {
-            "message": "Celery worker worked, lived, and died.",
-        }
-        return {"statusCode": 200, "body": json.dumps(body)}
+        remaining_seconds = context.get_remaining_time_in_millis() / 1000.0
     except Exception as e:
-        if 'sentry' in available_extras:
-            logger.warning('Sending exception collected to Sentry client')
-            available_extras['sentry'].captureException()
+        logger.exception('Could not got remaining_seconds. Is the context right?')
+        remaining_seconds = 5 * 60 # 5 minutes by default
 
-        ### Err hook call
-        maybe_call_hook(ENVVARS['error_handler_call'], locals())
-        raise
-    finally:
-        logger.info('END: Handle request ID: %s', request_id)
-        ### 5th hook call
-        maybe_call_hook(ENVVARS['post_handler_call'], locals())
+    softlimit = remaining_seconds-30.0  # Poke the job 30sec before the abyss
+    hardlimit = remaining_seconds-15.0  # Kill the job 15sec before the abyss
 
-        if 'wdb' in available_extras:
-            available_extras['wdb']['stop_trace']()
+    if not hooks:
+        logger.debug('Fresh Celery worker. Attach hooks!')
+        hooks = attach_hooks()
+    else:
+        logger.debug('Old Celery worker. Already have hooks.')
+
+    # The Celery worker will start here. Will connect, take 1 (one) task,
+    # process it, and quit.
+    logger.debug('Spawning the worker(s)')
+    spawn_worker(
+        softlimit=softlimit if softlimit > 5 else None,
+        hardlimit=hardlimit if hardlimit > 5 else None,
+        loglevel='DEBUG',
+    )  # Will block until one task got processed
+
+    logger.debug('Cleaning up before exit')
+    body = {
+        "message": "Celery worker worked, lived, and died.",
+    }
+    return {"statusCode": 200, "body": json.dumps(body)}
 
 
 ### 3rd hook call
