7070deb13c43f28e0990fcf9e5054312193af7d6
Alan Justino <alan.justino@yahoo.com.br>
2018-06-04 17:15:39 -0300

    Oops. handler_wrapper() should receive available_extras

--- celery_serverless/handler_utils.py
+++ celery_serverless/handler_utils.py
@@ -40,51 +40,48 @@ def import_callable(name):
     return result if callable(result) else None
 
 
-def handler_wrapper(fn, available_extras):
-    @maybe_apply_sentry(available_extras)
-    @functools.wraps(fn)
-    def _handler(event, context):
-        request_id = '(unknown)'
-        try:
-            if 'wdb' in available_extras:
-                available_extras['wdb']['start_trace']()
-                # Will be True if CELERY_SERVERLESS_BREAKPOINT is defined.
-                # It is the preferred way to force a breakpoint.
-                if available_extras['wdb']['breakpoint']:
-                    import wdb
-                    wdb.set_trace()  # Tip: you may want to step into fn() near line 72 ;)
+def handler_wrapper(available_extras):
+    if callable(available_extras):
+        raise TypeError("Should initialize the decorator with 'available_extras' map, not %s", type(available_extras))
 
-            ### 4th hook call
-            maybe_call_hook(ENVVAR_NAMES['pre_handler_call'], locals())
-
+    def _decorator(fn):
+        @maybe_apply_sentry(available_extras)
+        @functools.wraps(fn)
+        def _handler(event, context):
+            request_id = '(unknown)'
             try:
-                request_id = context.aws_request_id
-            except AttributeError:
-                pass
-            logger.info('START: Handle request ID: %s', request_id)
+                if 'wdb' in available_extras:
+                    available_extras['wdb']['start_trace']()
+                    # Will be True if CELERY_SERVERLESS_BREAKPOINT is defined.
+                    # It is the preferred way to force a breakpoint.
+                    if available_extras['wdb']['breakpoint']:
+                        import wdb
+                        wdb.set_trace()  # Tip: you may want to step into fn() near line 70 ;)
 
-            try:
-                remaining_seconds = context.get_remaining_time_in_millis() / 1000.0
+                ### 4th hook call
+                maybe_call_hook(ENVVAR_NAMES['pre_handler_call'], locals())
+
+                try:
+                    request_id = context.aws_request_id
+                except AttributeError:
+                    pass
+                logger.info('START: Handle request ID: %s', request_id)
+
+                return fn(event, context)
             except Exception as e:
-                logger.exception('Could not got remaining_seconds. Is the context right?')
-                remaining_seconds = 5 * 60 # 5 minutes by default
+                if 'sentry' in available_extras:
+                    logger.warning('Sending exception collected to Sentry client')
+                    available_extras['sentry'].captureException()
 
-            result = fn(event, context)
-            logger.debug('Cleaning up before exit')
-            return result
-        except Exception as e:
-            if 'sentry' in available_extras:
-                logger.warning('Sending exception collected to Sentry client')
-                available_extras['sentry'].captureException()
+                ### Err hook call
+                maybe_call_hook(ENVVAR_NAMES['error_handler_call'], locals())
+                raise
+            finally:
+                logger.info('END: Handle request ID: %s', request_id)
+                ### 5th hook call
+                maybe_call_hook(ENVVAR_NAMES['post_handler_call'], locals())
 
-            ### Err hook call
-            maybe_call_hook(ENVVAR_NAMES['error_handler_call'], locals())
-            raise
-        finally:
-            logger.info('END: Handle request ID: %s', request_id)
-            ### 5th hook call
-            maybe_call_hook(ENVVAR_NAMES['post_handler_call'], locals())
-
-            if 'wdb' in available_extras:
-                available_extras['wdb']['stop_trace']()
-    return _handler
+                if 'wdb' in available_extras:
+                    available_extras['wdb']['stop_trace']()
+        return _handler
+    return _decorator
