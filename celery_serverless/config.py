f6f4ede8d50c769d6926c130ed9ff13161aa67bd
Alan Justino <alan.justino@yahoo.com.br>
2018-06-07 16:15:20 -0300

    Fix py35 build

--- celery_serverless/config.py
+++ celery_serverless/config.py
@@ -11,6 +11,6 @@ yaml = YAML()
 @functools.lru_cache(1)
 def get_config(file_path='./serverless.yml'):
     if not os.path.exists(file_path):
-        raise RuntimeError(f"No file '{file_path}' detected."
+        raise RuntimeError("No file '%s' detected." % file_path +
                            " Please run 'celery serverless init' to create one if not exists")
     return yaml.load(Path(file_path))
