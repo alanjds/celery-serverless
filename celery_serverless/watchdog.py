e0fe08ada0228f6db81c855085c56c771b645e21
Alan Justino <alan.justino@yahoo.com.br>
2018-06-11 19:45:24 -0300

    Kombu queue length

--- celery_serverless/watchdog.py
+++ celery_serverless/watchdog.py
@@ -92,3 +92,53 @@ class StrippedLocMemCache(object):
         self._cache.setdefault(key, 0)
         self._cache[key] += delta
         return self.get(key)
+
+
+class KombuQueueLengher(object):
+    def __init__(broker_url):
+
+
+##
+# See: https://github.com/Skablam/kombu-examples/blob/master/robust-consumermixin.py
+from kombu import Connection, Exchange, Queue
+from kombu.mixins import ConsumerMixin
+
+rabbit_url = "amqp://localhost:5672/"
+
+
+class Worker(ConsumerMixin):
+    def __init__(self, connection, queues):
+        self.connection = connection
+        self.queues = queues
+
+    def get_consumers(self, Consumer, channel):
+        return [Consumer(queues=self.queues,
+                         callbacks=[self.on_message])]
+
+    def on_message(self, body, message):
+        print('Got message: {0}'.format(body))
+        message.ack()
+
+exchange = Exchange("example-exchange", type="direct")
+queues = [Queue("example-queue", exchange, routing_key="BOB")]
+
+with Connection(rabbit_url, heartbeat=4) as conn:
+    try:
+        worker = Worker(conn, queues)
+        worker.run()
+    except:
+        raise
+
+##
+        if hasattr(self.channel, '_size'):
+            # Redis
+            return sum(self.channel._size(queue) for queue in self.queues)
+        # AMQP
+        try:
+            from librabbitmq import ChannelError
+        except ImportError:
+            from amqp.exceptions import ChannelError
+        count = 0
+        for queue in self.queues:
+            try:
+                queue = self.channel.queue_declare(queue, passive=True)
