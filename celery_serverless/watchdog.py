8faa1f39e277bdde8fb815f8f70c2df7f024460f
sbneto <samuel.m.b.neto@gmail.com>
2018-06-20 17:28:39 -0300

    [WIP] no unconfirmed workers, simpler protocol, intercom keeps state

--- celery_serverless/watchdog.py
+++ celery_serverless/watchdog.py
@@ -1,10 +1,9 @@
 # coding: utf-8
 import time
+import uuid
 import logging
 import threading
-from pprint import pformat
 from functools import partial
-from collections import OrderedDict
 from concurrent.futures import as_completed
 from itertools import count
 from datetime import datetime, timezone, timedelta
@@ -34,93 +33,10 @@ class Watchdog(object):
         self.pool_size = 200
 
         # 0) Clear counters
-        self.workers_started = 0
-        self._unconfirmed_registry = OrderedDict()
-        self._unconfirmed_registry_lock = threading.Lock()
-        self._pubsub = self._intercom.pubsub() if isinstance(self._intercom, StrictRedis) else None
-        if self._pubsub:
-            self._init_pubsub()
-
-    def _init_pubsub(self):
-        """
-        Creates a bunch of callbacks for events sent by our workers.
-        """
-        channel_join_key = get_workers_all_key(prefix=self._name) + '[join]'
-        def _handle_join_event(message):
-            logging.debug('[event:join] Worker joined')
-            self.workers_started += 1
-
-        channel_working_key = get_workers_all_key(prefix=self._name) + '[working]'
-        def _handle_working_event(message):
-            logging.debug('[event:working] Worker got a job')
-            self.confirm_worker()
-
-        channel_leave_key = get_workers_all_key(prefix=self._name) + '[leave]'
-        def _handle_leave_event(message):
-            logging.debug('[event:leave] Worker rested')
-
-        self._subscription_hooks = {    # Prevents GC. Should hold the handle.
-            channel_join_key: _handle_join_event,
-            channel_working_key: _handle_working_event,
-            channel_leave_key: _handle_leave_event,
-        }
-        self._pubsub.subscribe(**self._subscription_hooks)
-        self._pubsub.run_in_thread(daemon=True)
-
-    def register_unconfirmed_workers(self, how_many=1):
-        assert how_many >= 0, 'do _confirm_worker instead of register negatives'
-        now = datetime.now().replace(microsecond=0)  # Capped to seconds buckets
-        self._unconfirmed_registry.setdefault(now, 0)
-        self._unconfirmed_registry[now] += how_many
-
-    def confirm_worker(self):
-        with self._unconfirmed_registry_lock:
-            now = datetime.now()
-            time = None
-            expired = []
-            for time, count in self._unconfirmed_registry.items():
-                if time + timedelta(**UNCONFIRMED_LIMIT) < now:
-                    # This register is expired.
-                    expired.append(time)
-                    continue
-                # Found!
-                break
-
-            # Clean the expired ones
-            for k in expired:
-                self._unconfirmed_registry.pop(k)
-
-            if time is None:
-                # No register found or all already expired
-                return False
-
-            # Consume the found stuff.
-            if count > 1:
-                self._unconfirmed_registry[time] -= 1
-            else:  # 1 or less
-                self._unconfirmed_registry.pop(time)
-
-            return True
-
-    def get_workers_unconfirmed(self):
-        logger.debug('_unconfirmed_registry: %s', pformat(dict(self._unconfirmed_registry)))
-
-        now = datetime.now()
-        valid = 0
-        invalid = []
-        for time, count in self._unconfirmed_registry.items():
-            if time + timedelta(**UNCONFIRMED_LIMIT) >= now:
-                # Count up the valid times key values
-                valid += count
-            else:
-                invalid.append(time)
-
-        # Clear the expired times keys
-        for k in invalid:
-            self._unconfirmed_registry.pop(k)
-        return valid
+        self.joined_event_count = 0
 
     def get_workers_count(self):
+        raise NotImplementedError()
         if hasattr(self._intercom, 'get_workers_count'):
             return self._intercom.get_workers_count()
         return refresh_workers_all_key(self._intercom)[0]
@@ -137,14 +53,39 @@ class Watchdog(object):
     # Actions:
     #
 
-    def trigger_workers(self, how_much:int):
-        logger.info('Starting %s workers', how_much)
+    def _inform_new_worker(self, worker_id: str):
+        """
+        Inform the central state in self._intercom that a new worker joined.
+        Sets the expiration of the state.
+        """
+        raise NotImplementedError('Create a new redis key and set the expiration')
+        if isinstance(self._intercom, MuteIntercom):
+            return None
+
+        with self._intercom.pipeline() as pipe:
+            pipe.sadd(bucket, worker_id)
+            pipe.expire(bucket, DEFAULT_BUCKET_EXPIRE)
+            pipe.execute()
+        return key
+
+    def trigger_worker(self):
+        """
+        Generates a new worker id, adds a REDIS key with this id and the current timestamp
+        and invokes the worker.
+        :return: A tuple: (Triggered worker ID,) + invoke_worker()
+        """
+        worker_uuid = uuid.uuid1()
+        self._inform_new_worker(worker_uuid)
+        return invoke_worker(worker_id=worker_uuid, worker_trigger_time=datetime.now()) + (worker_uuid,)
+
+    def trigger_workers(self, how_many:int):
+        if not how_many:
+            return 0
+        logger.info('Starting %s workers', how_many)
         success_calls = 0
         invocations = []
-        for i in range(how_much):
-            triggered, future = invoke_worker()
-            if triggered:
-                self.register_unconfirmed_workers(1)
+        for i in range(how_many):
+            triggered, future, worker_id = self.trigger_worker()
             invocations.append(future)
 
         for future in as_completed(invocations):
@@ -152,7 +93,6 @@ class Watchdog(object):
                 future.result()
                 success_calls += 1
             except Exception as err:
-                self.confirm_worker()  # Confirmed as failed
                 logger.error('Invocation failed', exc_info=1)
 
         return success_calls
@@ -166,33 +106,19 @@ class Watchdog(object):
 
             # 2a) Stop if empty queue and no running worker left
             existing_workers = self.get_workers_count()
-            if not queue_length:
-                if existing_workers:
-                    logger.debug('Empty queue, but still %s workers running', existing_workers)
-                else:  # No queue and no workers: Stop monitoring
-                    logger.debug('Empty queue and no worker running. Stop monitoring')
-                    unconfirmed = self.get_workers_unconfirmed()
-                    if unconfirmed:
-                        logger.warning('Exiting with %s still unconfirmed workers!', unconfirmed)
-                    break
+            if not queue_length and not existing_workers:
+                logger.debug('Empty queue and no worker running. Stop monitoring')
+                break
+
+            logger.debug('We have %s enqueued tasks and %s workers running', queue_length, existing_workers)
 
             # 2b) Start (N-existing) workers
-            unconfirmed = self.get_workers_unconfirmed()
-            available_workers = self.pool_size - existing_workers - unconfirmed
+            available_workers = self.pool_size - existing_workers
             available_workers = max(available_workers, 0)
-            unhandled_queued = queue_length - unconfirmed
 
-            to_trigger = min(unhandled_queued, available_workers)
-            if to_trigger > 0:
-                self.trigger_workers(to_trigger)
+            self.trigger_workers(available_workers)
 
-            # 3b) Watch for N starts
-            # started = self.wait_start_notifications(triggered)
-
-            # 4) Watch for N working notifications
-            # working = self.wait_working_notifications(started)
-
-        return self.workers_started  # How many had to be started to fulfill the queue?
+        return self.joined_event_count  # How many had to be started to fulfill the queue?
 
 
 class MuteIntercom(object):
@@ -241,19 +167,6 @@ class KombuQueueLengther(object):
         return result
 
 
-def _truncate_to_minute(now):
-    return now.replace(second=0, microsecond=0)
-
-
-def get_workers_all_key(prefix=DEFAULT_BASENAME):
-    return '%s:workers:all' % prefix
-
-
-def get_workers_bucket_key(prefix=DEFAULT_BASENAME, now=None):
-    this_minute = _truncate_to_minute(now or datetime.now(timezone.utc))
-    return '%s:workers:%s' % (prefix, this_minute.isoformat())
-
-
 def build_intercom(intercom):
     if not intercom or intercom == 'disabled':
         return MuteIntercom()
@@ -263,37 +176,26 @@ def build_intercom(intercom):
         raise NotImplementedError()
 
 
-def inform_worker_join(redis:'StrictRedis', worker_id:str, bucket='', prefix=DEFAULT_BASENAME, now=None):
-    if isinstance(redis, MuteIntercom):
-        return None
-
-    bucket = bucket or get_workers_bucket_key(prefix=prefix, now=now)
-    with redis.pipeline() as pipe:
-        pipe.sadd(bucket, worker_id)
-        pipe.expire(bucket, DEFAULT_BUCKET_EXPIRE)
-        pipe.publish(get_workers_all_key(prefix=prefix) + '[join]', worker_id)
-        pipe.execute()
-    return bucket
-
-
-def inform_worker_working(redis:'StrictRedis', worker_id:str, prefix=DEFAULT_BASENAME):
-    if isinstance(redis, MuteIntercom):
-        return None
-    return redis.publish(get_workers_all_key(prefix=prefix) + '[working]', worker_id)
-
-
-def inform_worker_leave(redis:'StrictRedis', worker_id:str, bucket:str):
+def inform_worker_leave(redis:'StrictRedis', worker_id:str):
+    raise NotImplementedError()
     if isinstance(redis, MuteIntercom):
         return None
 
     with redis.pipeline() as pipe:
-        pipe.srem(bucket, worker_id)
-        pipe.publish(get_workers_all_key() + '[leave]', worker_id)
+        pipe.srem((get_workers_all_key(), worker_id))
         was_removed, _ = pipe.execute()
     return was_removed
 
 
 def refresh_workers_all_key(redis:'StrictRedis', prefix=DEFAULT_BASENAME, now=None, minutes=5):
+    raise NotImplementedError()
+
+    def get_workers_all_key(prefix=DEFAULT_BASENAME):
+        return '%s:workers:all' % prefix
+
+    def get_workers_bucket_key(prefix=DEFAULT_BASENAME, now=None):
+        return '%s:workers:%s' % (prefix, this_minute.isoformat())
+
     if isinstance(redis, MuteIntercom):
         return None, None, None
 
