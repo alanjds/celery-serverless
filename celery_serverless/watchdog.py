8fb84e87464be2217de9678a3bba42d573057674
Alan Justino <alan.justino@yahoo.com.br>
2018-06-27 14:45:18 -0300

    Debug of inform[] events

--- celery_serverless/watchdog.py
+++ celery_serverless/watchdog.py
@@ -213,6 +213,7 @@ def inform_worker_new(redis:'StrictRedis', worker_id:str, prefix=DEFAULT_BASENAM
         pipe.expire(workers_started_key, DEFAULT_WORKER_EXPIRE)  # Renew expire limit
         result, *_ = pipe.execute()
 
+    logger.debug('Informed [new]: %s', worker_key)
     return (worker_key, metadata) if result else result
 
 
@@ -235,6 +236,8 @@ def inform_worker_busy(redis:'StrictRedis', worker_id:str, prefix=DEFAULT_BASENA
         pipe.expire(workers_busy_key, DEFAULT_WORKER_EXPIRE)
         pipe.expire(workers_started_key, DEFAULT_WORKER_EXPIRE)
         result, *_ = pipe.execute()
+
+    logger.debug('Informed [busy]: %s', worker_key)
     return result
 
 
@@ -252,6 +255,8 @@ def inform_worker_leave(redis:'StrictRedis', worker_id:str, prefix=DEFAULT_BASEN
         pipe.zrem(workers_started_key, worker_key)
         pipe.zrem(workers_busy_key, worker_key)
         _, *deleted = pipe.execute()
+
+    logger.debug('Informed [leave]: %s', worker_key)
     return len(deleted)
 
 
